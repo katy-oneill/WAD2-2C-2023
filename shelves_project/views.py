da284cd0f2e2f56a58f12e0cea43e5e637e1bbf5
Katy O'Neill <2563183o@student.gla.ac.uk>
2023-03-06 16:30:25 +0000

    User authentication, database/population script initialised, templates started

--- shelves_project/views.py
+++ shelves_project/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse
+def launch(request):
+    return render(request, shelves_project/launch.html)
 
 # Create your views here.
