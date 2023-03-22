0c7af2b85665da0de913a1c2091334623e7c22cd
chenyao WU <2601386W@student.gla.ac.uk>
2023-03-22 01:11:29 +0000

    update URLs and views

--- shelves_project/urls.py
+++ shelves_project/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from shelves_project import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,4 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.launch, name='launch'),
     path('admin/', admin.site.urls),
+    path('friends/', views.friends, name='friends'),
+    path('personal/', views.personal, name='personal'),
+    path('social/', views.social, name='social'),
 ]
--- shelves_project/views.py
+++ shelves_project/views.py
@@ -6,3 +6,12 @@ def launch(request):
 
 def index(request):
     return HttpResponse("Hey!")
+
+def friends(request):
+    return render(request, 'shelves/friends.html')
+
+def personal(request):
+    return render(request, 'shelves/personal.html')
+
+def social(request):
+    return render(request, 'shelves/social.html')
